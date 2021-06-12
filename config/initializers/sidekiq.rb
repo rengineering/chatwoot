ece05aafe6e133cb56305acec576d14eb641f6c7
Ani Ravi <5902976+aniravi24@users.noreply.github.com>
2021-06-12 08:44:44 -0700

    try changing redis config might need to revert

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,11 @@
 schedule_file = 'config/schedule.yml'
 
 Sidekiq.configure_client do |config|
-  config.redis = Redis::Config.sidekiq
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = Redis::Config.sidekiq
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
 end
 
 # https://github.com/ondrejbartas/sidekiq-cron
